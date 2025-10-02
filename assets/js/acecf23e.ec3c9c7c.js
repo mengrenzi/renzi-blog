273740d3a7f13f48a804dd37cf143d2d2be23576
mengrenzi <mengrenzi@users.noreply.github.com>
2025-10-02 21:12:19 +0000

    deploy: c0113540e3d5b9aa8521e1572a4555315f3bfb29

---
+++ assets/js/acecf23e.ec3c9c7c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_blog=self.webpackChunkmy_blog||[]).push([[1903],{1912:l=>{l.exports=JSON.parse('{"blogBasePath":"/renzi-blog/blog","blogTitle":"Blog","authorsListPath":"/renzi-blog/blog/authors"}')}}]);
---
+++ assets/js/f370f241.5087664f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_blog=self.webpackChunkmy_blog||[]).push([[3922],{8165:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/renzi-blog/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
